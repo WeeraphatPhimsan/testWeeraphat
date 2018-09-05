567d17f345ef00c41b7d9ed3530d1ca9e671810c
WeeraphatPhimsan <42836912+WeeraphatPhimsan@users.noreply.github.com>
2018-09-05 14:59:24 +0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+onload = init();
+
+function init() {
+
+	// var name;
+	// name = prompt("What is your name ?");
+	// age = prompt("How old are you ?");
+	// if(name!=null){
+	// 	alert('Welcome! ' + name);
+	// }
+	// if(age!=null){
+	// 	alert('You are ' + age + ' year old.')
+	// }
+
+}
+
